e10fc89bad481982440aeca109954733c090c4aa
Arifayan Idowu Olatubosun <arifayanidowu2@yahoo.com>
2019-11-21 21:50:16 +0100

    Updated app.js

--- pages/_app.js
+++ pages/_app.js
@@ -59,10 +59,10 @@ export default class MyApp extends App {
 
   componentDidMount() {
     //Remove the server-side injected CSS.
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
+    // const jssStyles = document.querySelector("#jss-server-side");
+    // if (jssStyles) {
+    //   jssStyles.parentElement.removeChild(jssStyles);
+    // }
   }
 
   render() {
